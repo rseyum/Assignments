ddd3e1fd15858c4061089ada28d4761b81431516
rseyum <rseyum@sea.yearup.org>
2022-05-31 13:51:52 -0700

    some js

--- java-walk/domwalkthrough.js
+++ java-walk/domwalkthrough.js
@@ -15,4 +15,37 @@
 // console.log(document.all[13]);
 // document.all[18].textContent="we just changed the header"
  //acessing the DOM withgetelementbyid
+// document object can scan thr tree for element
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
+// console.log(document.getElemtnById("header-title");
+//creation of the object variable using the doucmetn function
+// let headerTitle=document.getElementById("header-title");
+// headerTitle.textContent="Im Changing the Header"
+// headerTitle.text.inner="I am changing it again"
+//headerTitle.style.color="hotpink"
+var items=document.getElementByIdClassName("list-group-item");
+console.log(itmes);
+console.log(items[1]);
+
+items[1].style.fontweight="bold"
+items[1].style.backgroundColor="yellow"
+items[1].style.textContent="Alpha Kappa Alpha"
+items[1].style.Color="green"
+
+items[0].style.textContent="Delta Sigma Theta"
+items[0].style.fontweight="bold"
+items[0].style.backgroundColor="red"
+items[0].style.textContent="Oooop -Coleman Love"
+items[0].style.Color="brown"
+
+
+
+
+items[2].style.fontweight="bold"
+items[2].style.backgroundColor="purple"
+items[2].style.textContent="Zeela Zelda Zelda"
+items[2].style.Color="white"
+
  
