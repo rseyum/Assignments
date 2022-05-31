e452d4ccea825abe2f8d0612ca745409d341d6db
rseyum <rseyum@sea.yearup.org>
2022-05-31 12:10:38 -0700

    first commit

---
+++ java-walk/domwalkthrough.js
@@ -0,0 +1,18 @@
+// console.dir(document.domain);
+// // 
+// console.log(document.URL);
+// // 
+// console.log(document.title);
+// // 
+// console.log(documnet.doctype);
+// // 
+// console.log(document.head);
+// // 
+// console.log(document.body);
+// // 
+// console.log(document.all);
+// // 
+// console.log(document.all[13]);
+// document.all[18].textContent="we just changed the header"
+ //acessing the DOM withgetelementbyid
+ 
