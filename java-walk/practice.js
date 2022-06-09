bc5931310e40a13642e21db58f308c31405c19ce
rseyum <rseyum@sea.yearup.org>
2022-06-09 01:14:15 -0700

    second push

--- java-walk/practice.js
+++ java-walk/practice.js
@@ -1,9 +1,4 @@
 const dayOfweek="monday";
-
-
-
-
-
 const age= prompt('Enter An Age To Drive');
 
 // if condition is used to specsify a block of code to be excuted , if a specified condition is true 
@@ -19,7 +14,6 @@ else if (age<10){
 else(){
 
 }
-
 
 const expr='Papayas';
 switch(expr){
@@ -38,6 +32,8 @@ console.log ("sorry we are out of your asked item")
 // switch case is same as if and else condition but incase of many 
 // alternative block to be exuted is used.the default keyword must not be at 
 // the end of our code all the time ,lastNamcan be 
+
+
 const password= prompt ("please enter a new password");
 if (password.length >=6){
     
@@ -58,7 +54,7 @@ function singSong(){
     console.log("RE");
     console.log("MI");
 }
-singSong 
+singSong()
 // its a function name for the function, it should be 
 // called out side the function in order to excute and it should allow js identification
 //  rule that means it should be in lowerCase, and templates literal the back ticks helps to 
@@ -74,6 +70,7 @@ function greet(firstName){
     
 //interpolation is an automatic replacing of 
 //variable with a real value we want symbolize
+
 function greet1(firstName,lastName){
     console.log(`hey there, ${firstName} ${lastName}`)
         
