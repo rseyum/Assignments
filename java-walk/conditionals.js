4f96d0f620e56cfb566499b41af093913de78ca2
rseyum <rseyum@sea.yearup.org>
2022-06-13 18:37:49 -0700

    updates comment

--- java-walk/conditionals.js
+++ java-walk/conditionals.js
@@ -1,97 +1,99 @@
-        console.log("Hello World");
-        let randomNumber = Math.random();
-        /*math.random methods generets random numbers between
-         This function returns a number in the range 0 to less than 1*/
+console.log("Hello World");
+let randomNumber = Math.random();
+/*math.random methods generets random numbers between
+        This function returns a number in the range 0 to less than 1*/
 
-        if (randomNumber < 0.5) {
-        console.log("Condition was Met");
-        console.log(randomNumber);
-        }
-        /*"if" is used to specify a part of code to be excuted if a specified condition is true
+if (randomNumber < 0.5) {
+  console.log("Condition was Met");
+  console.log(randomNumber);
+}
+/*"if" is used to specify a part of code to be excuted if a specified condition is true
         the if statement excutes one set of code if a specified conditon is met (true) or another
         set of code evaluates to false*/
 
-        if (randomNumber >= 0.5) {
-        console.log("Condition Was Met");
-        console.log(randomNumber);
-        }
+if (randomNumber >= 0.5) {
+  console.log("Condition Was Met");
+  console.log(randomNumber);
+}
+// Condition was Met will be return if if the condition matches
 
-
-        const dayOfWeek = "Saturday";
-        if (dayOfWeek === "Monday") {
-        console.log("Yee its Monday!");
-        } else if (dayOfWeek === "Friday") {
-        console.log("Yee Its Friday");
-        } else if (dayOfWeek === "Saturday") {
-        console.log("You have the correct Day from the variable");
-        }
-        /*else if is used to specify a new condition to test if,the first conditon is false
+const dayOfWeek = "Saturday";
+if (dayOfWeek === "Monday") {
+  console.log("Yee its Monday!");
+} else if (dayOfWeek === "Friday") {
+  console.log("Yee Its Friday");
+} else if (dayOfWeek === "Saturday") {
+  console.log("You have the correct Day from the variable");
+}
+/*else if is used to specify a new condition to test if,the first conditon is false
         else is used to specify a part of code to be excuted ,if the first codition is false
         if condition is used to specify a block of code to be excuted , if a specified condition is true
         switch case:many altrnative block to be excuted*/
 
-        if (age < 5) {
-        console.log("You Type pretty fast -5");
-        } else if (age < 10) {
-        console.log("hey , watch kids movie");
-        } else {
-        console.log("password is too short");
-        }
-    /*here we are promting the user to enter a age
-    then it will check that age with the series of if statements
-    and if age matches to a certain group, it will print out the 
-    coressponding message*/
-        const expr = "Papayas";
-        switch (expr) {
-        case "Oranges":
-            console.log("orange are $o.59 a pound");
-            break;
-        case "Bananas":
-            console.log("Hello Bananas");
-            break;
-        case "Avocados":
-            console.log("Hello Avocados");
-            break;
-        default: //
-            console.log("sorry we are out of your asked item");
-        }
-        /* we will get an out put "sorry we are out of your asked item" because there is no match for Papayas
-         switch case is same as if and else condition but incase of many
-         alternative block to be exuted is used.the default keyword must not be at
-         the end of our code all the time ,it can be in the first of the code
-         the break helps to end processing of a statement with in the switch case statement*/
-        const password = prompt("Please Enter a New Password");
-        if (password.length >= 6) {
-        if (password.indexOf(" ") === -1) {
-            console.log("Valid Password");
-        } else {
-            console.log("Password Cant Have Spaces");
-        }
-        } else {
-        console.log("Password is too short");
-        }
+if (age < 5) {
+  console.log("You Type pretty fast -5");
+} else if (age < 10) {
+  console.log("hey , watch kids movie");
+} else {
+  console.log("password is too short");
+}
+/*here we are prompting the user to enter age, then it will check that age
+ with the series of if statements, and if age matches a certain group, it 
+ will print out the corresponding message*/
+const expr = "Papayas";
+switch (expr) {
+  case "Oranges":
+    console.log("orange are $o.59 a pound");
+    break;
+  case "Bananas":
+    console.log("Hello Bananas");
+    break;
+  case "Avocados":
+    console.log("Hello Avocados");
+    break;
+  default: //
+    console.log("sorry we are out of your asked item");
+}
+/* We will get an out put "sorry we are out of your asked item" 
+   because there is no match for Papayas switch case is same as if and
+   else condition but in case of many alternative blocks to be executed 
+   is used. The default keyword must not always be at the end of our code; 
+   it can be at the first of the code. The break helps to end the processing 
+   of a statement within the switch case statement.*/
+const password = prompt("Please Enter a New Password");
+if (password.length >= 6) {
+  if (password.indexOf(" ") === -1) {
+    console.log("Valid Password");
+  } else {
+    console.log("Password Cant Have Spaces");
+  }
+} else {
+  console.log("Password is too short");
+}
 
-        function singSong() {
-        console.log("DO");
-        console.log("RE");
-        console.log("MI");
-        }
-        singSong();
-        singSong();
-        singSong();
-         /*"singSong()" is a function call for the function, in this case we are claling three
-         times and we will get an out put of DO RE MI three times in new lines
-          templates literal or back ticks helps to use single or double quates in a string and also for creating multiple literals*/
+function singSong() {
+  console.log("DO");
+  console.log("RE");
+  console.log("MI");
+}
+singSong();
+singSong();
+singSong();
+/*"singSong()" is a function call for the function;
+   in this case, we are calling three times, and we will
+   get an out put of DO RE MI three times in new lines templates
+   literal or backticks helps to use single or double quotes in a 
+   string and also for creating multiple literals.*/
 
-        function greet(firstName) {
-        console.log(`hey there, ${firstName}`);
-        }
-        greet("Rahel");
-        /* we will get an out put  hey there Rahel
-        interpolation is an automatic replacing of
-        variable with a real value we want symbolize*/
-        function greet1(firstName, lastName) {
-        console.log(`hey there, ${firstName} ${lastName}`);
-        }
-        greet1("Rahel", "Seyum");
-        //we will get an out put hey there Rahel Seyum
+function greet(firstName) {
+  console.log(`hey there, ${firstName}`);
+}
+greet("Rahel");
+/* We will get an output; hey there Rahel interpolation is an automatic
+ replacement of a variable with an accurate value we want to symbolize.
+        */
+function greet1(firstName, lastName) {
+  console.log(`hey there, ${firstName} ${lastName}`);
+}
+greet1("Rahel", "Seyum");
+//we will get an out put hey there Rahel Seyum
--- java-walk/domwalkthrough.js
+++ java-walk/domwalkthrough.js
@@ -1,115 +1,115 @@
 
-    //console.dir(document); displays an interactive list of the properties of the specified javascript object
-    // console.log(document.domain);// shows IP adress
-    // console.log(document.URL); //shows URL  
-    // console.log(document.title);//shows the title of the document
-    // console.log(document.doctype);//Returns the document's doctype
-    // console.log(document.head);//Returns the <head> element
-    // console.log(document.body);//Returns the all the listed in the body element 
-    // console.log(document.all);// show every tree structure in the document
-    // console.log(document.all[03]);calling line 3 in the document
-    // document.all[10].textContent = 'We Just Changed The Header';// manipulates js using document attribute to change the header
-    // Document Object Model can scan the tree for elements
-    // console.log(document.forms);//will returns a collection of all forms with in a particular page(Returns all <form> elements) how many we have
-    // console.log(document.links);//Returns all <area> and <a> elements that have a href attribute
-    // console.log(document.images); //Returns the <head> element
+        //console.dir(document); displays an interactive list of the properties of the specified javascript object
+        // console.log(document.domain);// shows IP adress
+        // console.log(document.URL); //shows URL  
+        // console.log(document.title);//shows the title of the document
+        // console.log(document.doctype);//Returns the document's doctype
+        // console.log(document.head);//Returns the <head> element
+        // console.log(document.body);//Returns the all the listed in the body element 
+        // console.log(document.all);// show every tree structure in the document
+        // console.log(document.all[03]);calling line 3 in the document
+        // document.all[10].textContent = 'We Just Changed The Header';// manipulates js using document attribute to change the header
+        // Document Object Model can scan the tree for elements
+        // console.log(document.forms);//will returns a collection of all forms with in a particular page(Returns all <form> elements) how many we have
+        // console.log(document.links);//Returns all <area> and <a> elements that have a href attribute
+        // console.log(document.images); //Returns the <head> element
 
-    ///////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////
 
-    //console.log(document.getElementById('header-title')); //Find an element by element id
-    //Creation of an object variable using  the document fncunction 
-    // let headerTitle = document.getElementById('header-title');
-    // headerTitle.textContent = "Im changing the Header";//the headerTitle will be changed here
-    // headerTitle.textContent = "I am changing it again.";// still changing it to another title
-    // headerTitle.style.color = 'hotpink';//styling headerTitle to hotpink
+        //console.log(document.getElementById('header-title')); //Find an element by element id
+        //Creation of an object variable using  the document fncunction 
+        // let headerTitle = document.getElementById('header-title');
+        // headerTitle.textContent = "Im changing the Header";//the headerTitle will be changed here
+        // headerTitle.textContent = "I am changing it again.";// still changing it to another title
+        // headerTitle.style.color = 'hotpink';//styling headerTitle to hotpink
 
-    ////////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////
 
 
-    // let items = document.getElementsByClassName("list-group-item");//Find elements by class name "list-group-item"
-    //GetElementByClassName use variable object to declare list items, also utilizing arrays to locate more specific items w/in the class
-    // console.log(items);returns all items with the className "list-group-item" in the document
-    // console.log(items[0]);//returns the first item listed in the array
-    // items[0].textContent = "Hello, World!";// the content of first element of the array is set to Hello,World!
-    // items[0].style.fontWeight = 'bold';//the style of the content of the first element of the array is bold
-    // items[0].style.backgroundColor = 'white';//the style of the content of the first element of the array is set background of white
-    // items[0].textContent = "Oooop -Coleman Love";//the content of first element of the array is set to Oooop -Coleman Love
-    // items[0].style.color = "brown";// the style of the content of the first element of the array  is set to brown color
+        // let items = document.getElementsByClassName("list-group-item");//Find elements by class name "list-group-item"
+        //GetElementByClassName use variable object to declare list items, also utilizing arrays to locate more specific items w/in the class
+        // console.log(items);returns all items with the className "list-group-item" in the document
+        // console.log(items[0]);//returns the first item listed in the array
+        // items[0].textContent = "Hello, World!";// the content of first element of the array is set to Hello,World!
+        // items[0].style.fontWeight = 'bold';//the style of the content of the first element of the array is bold
+        // items[0].style.backgroundColor = 'white';//the style of the content of the first element of the array is set background of white
+        // items[0].textContent = "Oooop -Coleman Love";//the content of first element of the array is set to Oooop -Coleman Love
+        // items[0].style.color = "brown";// the style of the content of the first element of the array  is set to brown color
 
-    /////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////////////////////
 
-    // items[1].textContent = "Bye"; //the content of first index of the array is set to Bye
-    // items[1].style.fontweight="bold" //the style of the content of the first element of the array is bold
-    // items[1].style.backgroundColor="yellow" //the style of the content of the first element of the array is set background of yellow
-    // items[1].style.textContent="Alpha Kappa Alpha" //the content of first element of the array is set to Alpha Kappa Alpha
-    // items[1].style.Color="green" // the style of the content of the first element of the array  is set to green color
+        // items[1].textContent = "Bye"; //the content of first index of the array is set to Bye
+        // items[1].style.fontweight="bold" //the style of the content of the first element of the array is bold
+        // items[1].style.backgroundColor="yellow" //the style of the content of the first element of the array is set background of yellow
+        // items[1].style.textContent="Alpha Kappa Alpha" //the content of first element of the array is set to Alpha Kappa Alpha
+        // items[1].style.Color="green" // the style of the content of the first element of the array  is set to green color
 
-    /////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////////////////
 
-    // items[2].textContent = "Hi"; //the content of the array is set to Hi
-    // items[2].style.fontWeight = 'bold'; //the style of the content of the third element of the array is bold
-    // items[2].style.backgroundColor = 'blue'; //the style of the content of the third element of the array is set background of blue
-    // items[2].textContent = "Zeela Zelda Zelda";  //the content of third element of the array is set to Zeela Zelda Zelda
-    // items[2].style.color = "green"; the style of the content of the third element of the array  is set to green color
+        // items[2].textContent = "Hi"; //the content of the array is set to Hi
+        // items[2].style.fontWeight = 'bold'; //the style of the content of the third element of the array is bold
+        // items[2].style.backgroundColor = 'blue'; //the style of the content of the third element of the array is set background of blue
+        // items[2].textContent = "Zeela Zelda Zelda";  //the content of third element of the array is set to Zeela Zelda Zelda
+        // items[2].style.color = "green"; the style of the content of the third element of the array  is set to green color
 
-    /////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////////////////////
 
-    // for loop that decrements from 100 to 0.
-    //for (let i = 0; i < items.length; i++) {
-    //     items[i].style.backgroundColor = 'grey'; // background color is set to grey
-    //     items[i].textContent = "I Changed The List"; // the text content is changed to I Changed The List
-    //     items[i].style.color = 'green'; // color of items is set to green color
-    // }
+        // for loop that decrements from 100 to 0.
+        //for (let i = 0; i < items.length; i++) {
+        //     items[i].style.backgroundColor = 'grey'; // background color is set to grey
+        //     items[i].textContent = "I Changed The List"; // the text content is changed to I Changed The List
+        //     items[i].style.color = 'green'; // color of items is set to green color
+        // }
 
-    /////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////////////////////
 
-    // const myDiamonds = ['Snacky','Scary','Piggy','Pinchie','Lily','IttyBitty','Nibbles','Lanky','Kibbles'];
-    // for(let i = 8; i < myDiamonds.length; i++){
-    // console.log(i, myDiamonds[i]);
-    //}for loop helps in  iteration , so here we will have list of arrays starting from the index 8 
+        // const myDiamonds = ['Snacky','Scary','Piggy','Pinchie','Lily','IttyBitty','Nibbles','Lanky','Kibbles'];
+        // for(let i = 8; i < myDiamonds.length; i++){
+        // console.log(i, myDiamonds[i]);
+        //}for loop helps in  iteration , so here we will have list of arrays starting from the index 8 
 
-    /////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////////////////////
 
-    // let tag = document.getElementsByTagName('li');//Find elements by tag name
-    // console.log(tag);
-    // console.log(tag[1]); // returns the value at index of 1 
-    // tag[1].textContent = "Hello 2"; // changes the text to Hello 2
-    // tag[1].style.fontWeight = 'bold'; // fontwieght of the second element is set to bold
-    // tag[1].style.backgroundColor = 'crimson'; // the background color of the second element is set to crimson color
-    // tag[1].style.color = 'white'; // the second element of set to white color
+        // let tag = document.getElementsByTagName('li');//Find elements by tag name
+        // console.log(tag);
+        // console.log(tag[1]); // returns the value at index of 1 
+        // tag[1].textContent = "Hello 2"; // changes the text to Hello 2
+        // tag[1].style.fontWeight = 'bold'; // fontwieght of the second element is set to bold
+        // tag[1].style.backgroundColor = 'crimson'; // the background color of the second element is set to crimson color
+        // tag[1].style.color = 'white'; // the second element of set to white color
 
-    // //////////////////////////////////////////////////////////////////
+        // //////////////////////////////////////////////////////////////////
 
-    // let header = document.querySelector('#main-header');
-    // header.style.borderBottom = 'solid 4px hotpink'; // changing the attrubute with the border bottom to red color
-    // let input = document.querySelector('input'); // creates input 
-    // input.value = "Changed the Input Box Text"; // giving the value of input "Changed the Input Box Text"
+        // let header = document.querySelector('#main-header');
+        // header.style.borderBottom = 'solid 4px hotpink'; // changing the attrubute with the border bottom to red color
+        // let input = document.querySelector('input'); // creates input 
+        // input.value = "Changed the Input Box Text"; // giving the value of input "Changed the Input Box Text"
 
-    ////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
 
-    // let submit = document.querySelector("input[type='submit']");
-    // submit.value = "Send"; // the value of the variable submit is now Send
-    // let item = document.querySelector('.list-group-item'); //item is now storing values with clssName list-group-item
-    // item.style.color = 'blue'; // item is set to blue color
-    // let lastItem = document.querySelector('.list-group-item:last-child');
-    // lastItem.style.color = 'pink'; // lastItem is set to pink color
-    //var titles = document.querySelectorAll('.title'); // the variable titles stores all the values with className title
-    //console.log(titles);
+        // let submit = document.querySelector("input[type='submit']");
+        // submit.value = "Send"; // the value of the variable submit is now Send
+        // let item = document.querySelector('.list-group-item'); //item is now storing values with clssName list-group-item
+        // item.style.color = 'blue'; // item is set to blue color
+        // let lastItem = document.querySelector('.list-group-item:last-child');
+        // lastItem.style.color = 'pink'; // lastItem is set to pink color
+        //var titles = document.querySelectorAll('.title'); // the variable titles stores all the values with className title
+        //console.log(titles);
 
-    //////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////
 
-    //titles[0].textContent = 'Is this Rahel';// the value of the content will be Is this rahel
-    // querySelector returns all element 
-    // that that matches ewith a specifed  css selector and the querySelector method can only used to acess
-    // a single element
-    //let odd = document.querySelectorAll('li:nth-child(odd)'); // this selector matches every element that's the nth child of its parent
-    //let even = document.querySelectorAll('li:nth-child(even)');
-    //for (var i =0; i < odd.length; i++){
-        //odd[i].style.backgroundColor = 'grey'; 
-        //even[i].style.backgroundColor = 'green'; 
-        //even[i].style.color = 'whitesmoke'; 
-    //}
-    //here we are styling based on the number we listed is given green and grey color 
+        //titles[0].textContent = 'Is this Rahel';// the value of the content will be Is this rahel
+        // querySelector returns all element 
+        // that that matches ewith a specifed  css selector and the querySelector method can only used to acess
+        // a single element
+        //let odd = document.querySelectorAll('li:nth-child(odd)'); // this selector matches every element that's the nth child of its parent
+        //let even = document.querySelectorAll('li:nth-child(even)');
+        //for (var i =0; i < odd.length; i++){
+            //odd[i].style.backgroundColor = 'grey'; 
+            //even[i].style.backgroundColor = 'green'; 
+            //even[i].style.color = 'whitesmoke'; 
+        //}
+        //here we are styling based on the number we listed is given green and grey color 
 
 
 
